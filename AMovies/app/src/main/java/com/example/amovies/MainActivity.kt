23ccfc3a443ea68bb971131ce53013a165717da9
Nikita Garashchuk <ngarashchuk03@internet.ru>
2024-01-15 20:04:00 +0300

    Make fragments

--- AMovies/app/src/main/java/com/example/amovies/MainActivity.kt
+++ AMovies/app/src/main/java/com/example/amovies/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.amovies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.amovies.fragments.MovieDetailFragment
+import com.example.amovies.fragments.MovieListFragment
+import com.example.amovies.fragments.RootFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), RootFragment.Companion.Listener, MovieDetailFragment.Companion.Listener {
+
+    /*private val rootFragment = RootFragment().apply {
+        setListener(this@MainActivity)
+    }*/
+    private val rootFragment = RootFragment()
+    /*private val movieDetailFragment = MovieDetailFragment().apply {
+        setListener(this@MainActivity)
+    }*/
+    private val movieDetailFragment = MovieDetailFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if(savedInstanceState == null)
+            supportFragmentManager.beginTransaction().add(R.id.fragment_container, rootFragment).commit()
+    }
+
+    override fun goBack() {
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, rootFragment).commit()
+    }
+
+    override fun goDetail() {
+        supportFragmentManager.beginTransaction().addToBackStack(null).add(R.id.fragment_container, movieDetailFragment).commit()
     }
 }
--- AMovies/app/src/main/java/com/example/amovies/fragments/RootFragment.kt
+++ AMovies/app/src/main/java/com/example/amovies/fragments/RootFragment.kt
@@ -2,32 +2,31 @@ package com.example.amovies.fragments
 
 import android.content.Context
 import android.os.Bundle
-import android.os.Parcelable
 import android.util.Log
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
+import android.widget.FrameLayout
 import com.example.amovies.R
-import java.io.Serializable
 
-class MovieDetailFragment: Fragment() {
+class RootFragment : Fragment() {
 
     private var listener: Listener? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        return inflater.inflate(R.layout.fragment_movie_detail, container, false)
+        return inflater.inflate(R.layout.fragment_root, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        requireActivity().supportFragmentManager.beginTransaction().add(R.id.preview_movie, MovieListFragment()).commit()
 
-        view.findViewById<TextView>(R.id.back_tv).setOnClickListener {
-            Log.d("CheckStatusListener", "MovieDetailFragment::: ${listener.toString()}")
-            listener?.goBack()
+        view.findViewById<FrameLayout>(R.id.preview_movie).setOnClickListener {
+            Log.d("CheckStatusListener", "RootFragment::: ${listener.toString()}")
+            listener?.goDetail()
         }
     }
 
@@ -45,10 +44,9 @@ class MovieDetailFragment: Fragment() {
 /*    fun setListener(l: Listener){
         listener = l
     }*/
-
     companion object{
         interface Listener{
-            fun goBack()
+            fun goDetail()
         }
     }
 }
